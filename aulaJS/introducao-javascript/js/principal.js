a343aa80372b9b9f52fb4571742774dc74bf2279
VianaDias <matheusvianadias@gmail.com>
2022-08-05 18:02:28 -0300

    att-intro-js

--- aulaJS/introducao-javascript/js/principal.js
+++ aulaJS/introducao-javascript/js/principal.js
@@ -20,15 +20,18 @@ for(let i = 0; i < pacientes.length; i++){
     let pesoehvalido = true;
     let alturaehvalida = true;
     
-    if(peso < 0 || peso > 100){
+    if(peso < 0 || peso > 1000){
         console.log("peso invalido");
         pesoehvalido = false;
         tdimc.textContent = "peso invalido!"
+        paciente.classList.add("paciente-invalido")
     }
     if(altura < 0 || altura > 3.00){
         console.log("altura invalida")
         alturaehvalida = false;
-        tdimc.textContent = "altura invalida!"
+        tdimc.textContent = "altura invalida!";
+        paciente.classList.add("paciente-invalido")
+
     }
     
     if( alturaehvalida && pesoehvalido){
